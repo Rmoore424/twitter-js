b7898ff4337422f5dfa7603f8b48ad3996ea9753
Richard Moore <Rmoore424@gmail.com>
2015-01-23 15:36:16 -0500

    added swig module and setup index.html template

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 var express = require("express");
 var morgan = require("morgan");
 var app = express();
+var swig = require("swig");
 
+app.engine("html", swig.renderFile);
 app.use(morgan("dev"));
 
-//app.get("/", function(req, res) {
-//	res.send("Hello World");
-//});
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
+swig.setDefaults({ cache: false });
+
+app.get("/", function(req, res) {
+	var people = [{name: "Full"}, {name: "Stacker"}, {name: "Son"}];
+	res.render("index", {title: "Hall of Fame", people: people});
+});
 
 var server = app.listen(3000, function () {
 	var host = server.address().address;
