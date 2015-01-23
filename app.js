57f363bbcef1f26456d2f9273bc92b4c229c8c4d
Richard Moore <Rmoore424@gmail.com>
2015-01-23 16:14:47 -0500

    added underscore module and set up the front page route

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ var express = require("express");
 var morgan = require("morgan");
 var app = express();
 var swig = require("swig");
+var routes = require("./routes");
 
 app.engine("html", swig.renderFile);
 app.use(morgan("dev"));
+app.use("/", routes);
+app.use(express.static("public"));
 
 app.set("view engine", "html");
 app.set("views", __dirname + "/views");
 swig.setDefaults({ cache: false });
 
-app.get("/", function(req, res) {
-	var people = [{name: "Full"}, {name: "Stacker"}, {name: "Son"}];
-	res.render("index", {title: "Hall of Fame", people: people});
-});
+// app.get("/", function(req, res) {
+// 	var people = [{name: "Full"}, {name: "Stacker"}, {name: "Son"}];
+// 	res.render("index", {title: "Hall of Fame", people: people});
+// });
+
+
 
 var server = app.listen(3000, function () {
 	var host = server.address().address;
